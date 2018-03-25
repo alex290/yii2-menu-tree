db131582f259c0593903d7cfa0e5490b432940c9
alex290 <alexmar12@yandex.ru>
2018-03-25 11:13:54 +0300

    Fix Bug URL Sef (Url::to)

--- menu_tpl/menu.php
+++ menu_tpl/menu.php
@@ -1,14 +1,17 @@
 <?php
-    if (yii\helpers\Url::to([$category['link']]) == Yii::$app->request->url) {
+use yii\helpers\Url;
+
+$MenuFull = $category['link'];
+    if (Url::to([$MenuFull]) == Yii::$app->request->url) {
         $classLi = 'active';
     } else {
         $classLi = '';
     }
 ?>
 <?php if (!isset($category['childs'])): ?>
-    <li class="nav-item <?= $classLi ?>"><a class="nav-link" href="<?= $category['link'] ?>" <?= $category['attribute'] ?> ><?= $category['name'] ?></a></li>
+<li class="nav-item <?= $classLi ?>"><a class="nav-link" href="<?= Url::to([$MenuFull]) ?>" <?= $category['attribute'] ?> ><?= $category['name'] ?></a></li>
 <?php else: ?>
-    <li class="nav-item dropdown <?= $classLi ?>"><a href="<?= yii\helpers\Url::to([$category['link']]) ?>"  class="dropdown-toggle" data-hover="dropdown" data-delay="100" data-toggle="dropdown" aria-expanded="false"><?= $category['name'] ?> <b class="caret"></b></a>
+<li class="nav-item dropdown <?= $classLi ?>"><a href="<?= Url::to([$MenuFull]) ?>"  class="dropdown-toggle" data-hover="dropdown" data-delay="100" data-toggle="dropdown" aria-expanded="false"><?= $category['name'] ?> <b class="caret"></b></a>
         <ul  class="dropdown-menu">
             <?= $this->getMenuHtml($category['childs']) ?>
         </ul>
